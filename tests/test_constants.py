7aeebc61d660b6ce0caee1093b6630c37e705205
bacetiner <bacetiner@ucla.edu>
2026-02-03 18:20:15 -0800

    bc - Adding tests for constants

---
+++ tests/test_constants.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2025 The University of Washington
+#
+# This file is part of rapidtools.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# You should have received a copy of the BSD 3-Clause License along with
+# rapidtools. If not, see <http://www.opensource.org/licenses/>.
+#
+# Contributors:
+# Barbaros Cetiner
+#
+# Last updated:
+# 02-03-2026
+
+from rapidtools.constants import EARTH_RADIUS_KM, LATITUDE_SPACING_KM
+
+
+def test_earth_radius_constant():
+    """
+    Verify EARTH_RADIUS_KM constant.
+
+    Checks:
+    1. Value matches standard integer approximation (6371 km).
+    2. Data type is integer.
+    """
+    # Value check
+    assert EARTH_RADIUS_KM == 6371
+
+    # Type check
+    assert isinstance(EARTH_RADIUS_KM, int)
+
+def test_latitude_spacing_constant():
+    """
+    Verify LATITUDE_SPACING_KM constant.
+
+    Checks:
+    1. Value matches the standard approximation (111 km per degree).
+    2. Data type is integer.
+    """
+    # Value check
+    assert LATITUDE_SPACING_KM == 111
+
+    # Type check
+    assert isinstance(LATITUDE_SPACING_KM, int)
